e79e09e1ebd4093f6824194806c1631ef6d56827
hamishdickson <hamish.dickson@gmail.com>
2015-06-20 14:05:24 +0100

    first commit

---
+++ qec.py
@@ -0,0 +1,7 @@
+"""Quantum Error Correction
+"""
+
+class Qec():
+    def shor():
+        """Shor code corrects arbitary single-qubit errors"""
+        
---
+++ qec_test.py
@@ -0,0 +1,8 @@
+import qec
+
+def test_no_error():
+    qec = Qec()
+    print "hi"
+    
+if __name__ == "__main__":
+    test_no_error()
